6cb57be10016f17357abc01dd1278177593dc7f3
Mory Friedman <friedmanmory2@gmail.com>
2015-10-20 13:43:46 -0500

    Revised Tax-Calculator

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -9214,21 +9214,27 @@ return jQuery;
 'use strict';
 var $ = require('jquery');
 
-var $subtotal = $('#subtotal');
-var $tax = $('#tax');
-var $form = $('#form');
-var $submit = $('#submit');
+var subtotal = $('#subtotal');
+var tax = $('#tax');
+var form = $('#form');
+var submit = $('#submit');
 
 function calculate(e) {
 	e.preventDefault();
-	var $total = $subtotal * $tax;
 
-	if ($subtotal.val() > 0 && $tax.val() > 0) {
-		$total = true;
+	if (subtotal.val() > 0 && tax.val() > 0) {
+		var subtotalValue = parseFloat(subtotal.val());
+		var taxAmount = subtotalValue * parseFloat(tax.val());
+		var total = taxAmount + subtotalValue;
+		$('#total').val(total);
+	} else if (subtotal.val() < 0 || tax.val() < 0) {
+		$('#errors').text('You have entered a negative number');
+	} else if (typeof subtotal.val() === 'string' || typeof tax.val() === 'string') {
+		$('#errors').text('You have entered text');
 	}
 }
 
-$submit.on('click', calculate);
+submit.on('click', calculate);
 
 },{"jquery":1}]},{},[2])
 
